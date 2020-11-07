f6de609c840377a9a0ec3a06aa92d247efa31686
Abib-P <paulabib17@gmail.com>
2020-11-07 21:11:17 +0100

    add the function readInsideXml and update createRootXmlBasic for to create XML_basic list when needed
    Signed-off-by: Abib-P <paulabib17@gmail.com>

--- sources/c_files/read_xml.c
+++ sources/c_files/read_xml.c
@@ -7,7 +7,7 @@
 #include "../h_files/char_useful.h"
 
 XML_basic* readXml(File_information* fileInfo) {
-    char actualCharRead = firstCharacterAfterSpace(fileInfo);
+    char actualCharRead = getFirstCharacterAfterSpace(fileInfo);
     if(actualCharRead != '<')
     {
         char buffer_where[1000]; //TODO taille arbitraire
@@ -23,7 +23,33 @@ XML_basic* readXml(File_information* fileInfo) {
 
     XML_basic* xmlRootElement = createRootXmlBasic(fileInfo);
 
-    return NULL; //TODO finish the fonction
+    readInsideXml(fileInfo, xmlRootElement);
+
+    return xmlRootElement; //TODO finish the fonction
+}
+
+void readInsideXml(File_information* fileInfo, XML_basic* xmlParent) {
+    char actualCharRead = getFirstCharacterAfterSpace(fileInfo);
+    while(actualCharRead != '<' && actualCharRead != '>' && actualCharRead != EOF)
+    {
+        addCharacterToStringValue(xmlParent,actualCharRead);
+        actualCharRead = getNextCharacterInFile(fileInfo);
+        /* //todo user enter an predefined entities (&amp; for & or &quot; for " )
+         *if (actualCharRead == '&'){
+         *
+         * }
+        */
+    }
+    if(actualCharRead == '<')
+    {
+        char buffer_where[1000]; //TODO taille arbitraire
+        char buffer_error_value[1000]; //TODO taille arbitraire
+
+        sprintf(buffer_where,"%s at %d:%d",fileInfo->fileName ,fileInfo->actualLine, fileInfo->actualColumn);
+        sprintf(buffer_error_value, "\'<\' character is expected");
+        fileInfo->error = createError(buffer_where,buffer_error_value);
+        return NULL;
+    }
 }
 
 XML_basic *createXmlBasic(File_information *fileInfo) {
@@ -37,11 +63,29 @@ XML_basic *createRootXmlBasic(File_information *fileInfo) {
     XML_basic* result = malloc(sizeof(XML_basic));
 
     result->elementName = getElementName(fileInfo);
-    result->parent = NULL;
-    result->listCapacity = 10;
-    result->list = malloc(sizeof(XML_basic)*result->listCapacity);
-    result->listSize = 0;
-    result->value = NULL;
 
+    result->parent = NULL;
+
+    result->markerList = NULL;
+    result->markerCapacity = 0;
+    result->markerSize = 0;
+
+    result->value = NULL;
+    result->valueSize = 0;
+    result->valueCapacity = 0;
+
+    /*
+     * result->attributeList = NULL;
+     * result->attributeSize = 0;
+     * result->attributeCapacity = 0;
+     */
+
+    /*
+     * if(getFirstCharacterAfterSpace(fileInfo) != >){
+     *      result->attribute = getAttribute(fileInfo);
+     * }
+    */
+
+    printf("%c", getFirstCharacterAfterSpace(fileInfo)); //todo suppr
     return result;
 }
--- sources/h_files/read_xml.h
+++ sources/h_files/read_xml.h
@@ -2,23 +2,31 @@
 // Created by paula on 22/10/2020.
 //
 
+#ifndef I_M_IN_LOVE_WITH_THE_XML_READXML_H
+#define I_M_IN_LOVE_WITH_THE_XML_READXML_H
+
 #include <stdio.h>
 #include "error_gestion.h"
 #include "file_information.h"
 
-#ifndef I_M_IN_LOVE_WITH_THE_XML_READXML_H
-#define I_M_IN_LOVE_WITH_THE_XML_READXML_H
-
 typedef struct XML_basic{
     char* elementName ;
-    char* value ; //cData or pcData
+    char* value; //pcData
+    int valueSize;
+    int valueCapacity;
     struct XML_basic* parent;
-    struct XML_basic* list;
-    int listSize;
-    int listCapacity;
+    struct XML_basic* markerList;
+    /*
+     * struct Attribute attributeList;
+     * int attributeSize;
+     * int attributeCapacity;
+     */
+    int markerSize;
+    int markerCapacity;
 } XML_basic;
 
 XML_basic* readXml(File_information* fileInformation);
+void readInsideXml(File_information* fileInfo, XML_basic* xmlParent);
 XML_basic* createRootXmlBasic(File_information* fileInformation);
 XML_basic* createXmlBasic(File_information* fileInformation);
 
