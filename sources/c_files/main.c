14ba1929d9af9f3b27fed2b176d65b1e9f0e2fa6
Abib-P <paulabib17@gmail.com>
2020-11-16 21:49:01 +0100

    last update before new branch
    Signed-off-by: Abib-P <paulabib17@gmail.com>

--- sources/c_files/main.c
+++ sources/c_files/main.c
@@ -5,12 +5,13 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 //todo free actual malloc when returning an error (to not lose the opportunity)
+//todo pop error when reach end of file in a comment
 
 int main(int argc, char **argv) {
 #ifdef __unix__
     setenv("DISPLAY", "127.0.0.1:0", 1);
 #endif
-    //startWindow(argc,argv);
+    startWindow(argc,argv);
     launchXml("../resources/test.xml");
     launchDtd("../resources/myDtd.dtd");
     return EXIT_SUCCESS;
